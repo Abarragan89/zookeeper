84e8052be55299f4b3790c29722b1dc0d072573c
Anthony Barragan <anthony.bar.89@gmail.com>
2022-03-01 10:42:07 -0800

    added front-end for animals

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// serve all the necessary static front-end files
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
-
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -79,10 +80,10 @@ function createNewAnimal(body, animalsArray) {
     return animal;
 }
 
-
-
-
-
+app.get('/', (req, res) => {
+    // only job is to send a file. 'sendFile'.
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -103,6 +104,7 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
+    console.log(req.body);
     // set id based on what the next index of the array will be 
     req.body.id = animals.length.toString();
     // if any data in req.body is incorrect, send 400 error back
@@ -112,10 +114,16 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal)
     }
-    // add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animals)
 });
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
